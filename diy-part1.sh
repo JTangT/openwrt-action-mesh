4ed3f8cf3a09624080d1bd5e5bf9333404108deb
JTangT <66877137+JTangT@users.noreply.github.com>
2025-06-26 11:03:12 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,6 +18,8 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git modem https://github.com/FUjr/modem_feeds.git;main' >> feeds.conf.default
 
+cp -r airpi/* .
+
 #mkdir -p package/feeds/modem
 #git clone https://github.com/mdsdtech/5G-Modem-Packages
 #mv 5G-Modem-Packages/quectel_cm_5G package/feeds/modem
